344ceda56cb4fec9f40ae96ecc7fc6943446beea
Shubham Upadhyay <103402465+ShubhamVUpadhyay@users.noreply.github.com>
2024-01-11 16:12:39 +0530

    Update settings.py

--- money_management/settings.py
+++ money_management/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR , 'staticfiles_build','static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR ,'static')]
 STATIC_URL = '/static/'
 
 
